483a3edc92597aafdbd9803239f6d6bc7eb70670
Abdalaziz Rashid <abdalaziz.rashid@outlook.com>
2024-02-20 03:12:50 +0300

    Settings

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -1,26 +1,27 @@
 { config, pkgs, lib, ... }:
 
 let
-  fromGitHub = ref: repo: pkgs.vimUtils.buildVimPlugin {
-    pname = "${lib.strings.sanitizeDerivationName repo}";
-    version = ref;
-    src = builtins.fetchGit {
-      url = "https://github.com/${repo}.git";
-      ref = ref;
+  fromGitHub = ref: repo:
+    pkgs.vimUtils.buildVimPlugin {
+      pname = "${lib.strings.sanitizeDerivationName repo}";
+      version = ref;
+      src = builtins.fetchGit {
+        url = "https://github.com/${repo}.git";
+        ref = ref;
+      };
     };
-  };
   nixvim = import (builtins.fetchGit {
     url = "https://github.com/nix-community/nixvim";
     # If you are not running an unstable channel of nixpkgs, select the corresponding branch of nixvim.
     ref = "nixos-23.11";
   });
-in
+  user = builtins.getEnv ("USER");
 
-{
+in {
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
-  home.username = builtins.getEnv("USER");
-  home.homeDirectory = builtins.getEnv("HOME");
+  home.username = user;
+  home.homeDirectory = builtins.getEnv ("HOME");
 
   # This value determines the Home Manager release that your configuration is
   # compatible with. This helps avoid breakage when a new Home Manager release
@@ -94,15 +95,13 @@ in
   #
   #  /etc/profiles/per-user/aziz/etc/profile.d/hm-session-vars.sh
   #
-  home.sessionVariables = {
-    EDITOR = "nvim";
-  };
+  home.sessionVariables = { EDITOR = "nvim"; };
 
   programs.direnv.enable = true;
   programs.direnv.nix-direnv.enable = true;
-  programs.bash.enable = true;
+  programs.bash.enable = false;
   programs.fish.enable = true;
-  programs.zsh.enable = true;
+  programs.zsh.enable = false;
   programs.starship.enable = true;
 
   programs.git = {
@@ -119,7 +118,6 @@ in
     };
   };
 
-  
   imports = [
     # For home-manager
     nixvim.homeManagerModules.nixvim
@@ -127,19 +125,25 @@ in
     #nixvim.nixosModules.nixvim
     # For nix-darwin
     #nixvim.nixDarwinModules.nixvim
-#    "${home.homeDirectory}/.config/tmux.nix"
+    #    "${home.homeDirectory}/.config/tmux.nix"
   ];
   # Neovim settings
   programs.nixvim = {
     enable = true;
     globals = {
-      mapleader = ",";
+      mapleader = " ";
+      maplocalleader = " ";
     };
     options = {
       number = true;
       relativenumber = true;
       shiftwidth = 2;
+      updatetime = 100;
+      fileencoding = "utf-8";
     };
+    filetype.extension.typ = "typst";
+    highlight = { Comment.fg = "#ff0000"; };
+    #colorscheme.gruvbox = true;
     keymaps = [
       {
         key = ";";
@@ -151,55 +155,114 @@ in
         options.silent = true;
         action = "<cmd>!make<CR>";
       }
-#      {
-#        key = "jk";
-#        action = "<Esc>";
-#      }
+      #      {
+      #        key = "jk";
+      #        action = "<Esc>";
+      #      }
     ];
-  plugins.lsp = {
-    enable = true;
-    servers = {
-      lua-ls.enable = true;
-      pyright.enable = true;
-      rnix-lsp.enable = true;
-      html.enable = true;
-      tsserver.enable = true;
-    };
-    keymaps = {
-      diagnostic = {
-        "<leader>j" = "goto_next";
-        "<leader>k" = "goto_prev";
+    plugins = {
+       lsp = {
+       enable = true;
+        servers = {
+          lua-ls.enable = true;
+          pyright.enable = true;
+          rnix-lsp.enable = true;
+          html.enable = true;
+          tsserver.enable = true;
+        };
+        keymaps = {
+          diagnostic = {
+            "<leader>j" = "goto_next";
+            "<leader>k" = "goto_prev";
+          };
+          lspBuf = {
+            K = "hover";
+            gD = "references";
+            gd = "definition";
+            gi = "implementation";
+            gy = "type_definition";
+          };
+        };
       };
-      lspBuf = {
-        K = "hover";
-        gD = "references";
-        gd = "definition";
-        gi = "implementation";
-        gy = "type_definition";
+
+      harpoon = {
+        enable = true;
+        keymaps = {
+          addFile = "<leader>a";
+          toggleQuickMenu = "<C-e>";
+        };
+      };
+      telescope = {
+        enable = true;
+        defaults.file_ignore_patterns = [ "^.git/" ];
+#        keymaps = {
+#          "<leader>ff" = "find_file";
+#          "<leader>fg" = "live_grep";
+#          "<leader>fb" = "buffers";
+#          "<leader>fh" = "help_tags";
+#          "<C-p>" = {
+#            action = "git_files";
+#            desc = "Telescope Git Files";
+#          };
+#        };
       };
     };
-};
-  extraPlugins = with pkgs.vimPlugins; [
+    extraPlugins = with pkgs.vimPlugins; [
       ctrlp
       editorconfig-vim
-      #gruvbox
       nerdtree
       tabular
       vim-nix
       vim-markdown
-     # nvim-lspconfig
       nvim-treesitter.withAllGrammars
       plenary-nvim
-      #gruvbox-material
-      (fromGitHub "HEAD" "nvim-telescope/telescope.nvim")
     ];
-  extraConfigLua = ''
-    -- Telescope recommended keybindings 
-    local builtin = require('telescope.builtin')
-    vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-    vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-    vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-    vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+    extraConfigLua = ''
+
+      -- Global mappings.
+      -- See `:help vim.diagnostic.*` for documentation on any of the below functions
+      vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
+      vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
+      vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+      vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
+      
+      -- Use LspAttach autocommand to only map the following keys
+      -- after the language server attaches to the current buffer
+      vim.api.nvim_create_autocmd('LspAttach', {
+        group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+        callback = function(ev)
+          -- Enable completion triggered by <c-x><c-o>
+          vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+      
+          -- Buffer local mappings.
+          -- See `:help vim.lsp.*` for documentation on any of the below functions
+          local opts = { buffer = ev.buf }
+          vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+          vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+          vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+          vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+          vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+          vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
+          vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
+          vim.keymap.set('n', '<space>wl', function()
+            print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+          end, opts)
+          vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
+          vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
+          vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
+          vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+          vim.keymap.set('n', '<space>f', function()
+            vim.lsp.buf.format { async = true }
+          end, opts)
+        end,
+      })
+      
+      -- Telescope
+      local builtin = require('telescope.builtin')
+      vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+      vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+      vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+      vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
     '';
   };
   programs.tmux = {
@@ -214,25 +277,21 @@ in
       fpp
       {
         plugin = resurrect;
-        extraConfig =
-          let
-            resurrectPrograms' = [
-              "vi"
-              "~vim->vim"
-              "~nvim->nvim"
-              "less"
-              "more"
-              "man"
-            ];
-            sep = " ";
-            hasWhiteSpaces = p: (builtins.match ".*[ \n\r\t].*" p) != null;
-            escapeProg = p: if (hasWhiteSpaces p) then ''"${p}"'' else p;
-            resurrectPrograms = lib.concatMapStringsSep sep escapeProg resurrectPrograms';
-          in
-          ''
-            set -g @ressurect-processes '${resurrectPrograms}'
-            set -g @resurrect-strategy-vim 'session'
-          '';
+        extraConfig = let
+          resurrectPrograms' =
+            [ "vi" "~vim->vim" "~nvim->nvim" "less" "more" "man" ];
+          sep = " ";
+          hasWhiteSpaces = p:
+            (builtins.match ''
+              .*[ 
+              	].*'' p) != null;
+          escapeProg = p: if (hasWhiteSpaces p) then ''"${p}"'' else p;
+          resurrectPrograms =
+            lib.concatMapStringsSep sep escapeProg resurrectPrograms';
+        in ''
+          set -g @ressurect-processes '${resurrectPrograms}'
+          set -g @resurrect-strategy-vim 'session'
+        '';
       }
       {
         plugin = continuum;
@@ -243,14 +302,21 @@ in
       }
     ];
   };
- # TODO: what if this is defined in another file? Merge it!
-#  programs.fish = {
-#    enable = true;
-#    shellInit = ''
-#      fish_add_path ${t-smart-manager}/share/tmux-plugins/t-smart-tmux-session-manager/bin/
-#    '';
-#  };
-#
+  # TODO: what if this is defined in another file? Merge it!
+  #  programs.fish = {
+  #    enable = true;
+  #    shellInit = ''
+  #      fish_add_path ${t-smart-manager}/share/tmux-plugins/t-smart-tmux-session-manager/bin/
+  #    '';
+  #  };
+  #
   # Let Home Manager install and manage itself.
-  programs.home-manager.enable = true;
+  # users.users.${user} = { programs.fish.enable = true; };
+  programs.home-manager = {
+    enable = true;
+    # TODO: find a way to not hardcode the username
+    #   users.aziz = {
+    #     programs.fish.enable = true;
+    #   };
+  };
 }
